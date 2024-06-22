a43a7ed27b1362483a3496244875511cf23c9471
saurabh10022000 <saurabhjain4916@gmail.com>
2024-06-22 01:58:22 -0700

    all setup config

--- src/app.js
+++ src/app.js
@@ -11,3 +11,11 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+
+//routes import
+import userRouter from './routes/user.routes.js'
+app.use("/api/v1/users", userRouter)
+
+//routers declaration
+export {app}
